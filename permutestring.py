9876c8d95a470ff39f3eba01d7d30e267010c6d2
Ranjit Avasarala <ranjit.avasarala@gmail.com>
2021-04-25 23:01:41 -0500

    Add files via upload
    This code is to find odd and even occurrences of numbers in a given string
    e.g.  list is [2,2,2,3,3,4,4,5,5,5,7,7,7,7,7]
    then output is:  Num which have odd occurrences:  2,5,7
    Num which have even occurrences: 3,4

--- permutestring.py
+++ permutestring.py
@@ -1,18 +1,31 @@
-def toString(List):
-    return ''.join(List)
-
-def permute(a, l, r):
-    if l == r:
-        print(toString(a))
-    else:
-        for i in range(l, r + 1):
-            a[l], a[i] = a[i], a[l]
-            permute(a, l + 1, r)
-            a[l], a[i] = a[i], a[l]  # backtrack
+def findoccurances(list):
+    oddoccur = []
+    evenoccur = []
+    count = 1
+    item = 0
+    for item in range(len(list)-1):
+        if item+1 == len(list)-1:
+            # Handle last item
+            if list[item+1] == list[item-1]:
+                count += 1
+                if count % 2 != 0:
+                    oddoccur.append(list[item])
+                else:
+                    evenoccur.append(list[item])
+        if list[item] == list[item+1]:
+            count += 1
+        else:
+            if (count % 2) != 0:
+               oddoccur.append(list[item])
+            else:
+               evenoccur.append(list[item])
+              # reset count to 1
+            count = 1
+    print("Num which have odd occurances: ", oddoccur)
+    print("Num which have even occurances: ", evenoccur)
 
 # Driver program to test the above function
 if __name__ == "__main__":
-   string = "ABCD"
-   n = len(string)
-   a = list(string)
-   permute(a, 0, n - 1)
+   list = [2,2,2,3,3,4,4,5,5,5,7,7,7,7,7]
+   findoccurances(list)
+
